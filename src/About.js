f6c06d5cf5abcc719d5224aa0c2799ad54974572
Sandbom <Simon.sandbom@gmail.com>
2020-06-22 16:50:00 +0200

    Adding basic routing and more
    Adding routing, home, contact and about me pages along with banner picture and filler text

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export const About = () => {
+    return (
+        <div>
+            <h2>About Page</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis placerat nisl vitae sem efficitur, non ullamcorper nibh fermentum. Aliquam consectetur nunc est, at convallis mauris tristique eu. Curabitur a nulla ac nisi efficitur tincidunt ut elementum orci. Duis ac augue in tellus sodales luctus non eget orci. Sed commodo, eros eu cursus laoreet, ipsum lectus semper tortor, ut tincidunt nulla nisi in nulla. Sed pretium vestibulum purus, in consequat augue dapibus sit amet. Aenean aliquam, est ut maximus finibus, nibh est euismod eros, vel ultricies augue leo ut neque. Nullam eu est in est rhoncus feugiat. Sed lobortis lobortis ligula sed sagittis. Vestibulum sit amet volutpat ante. Quisque sed ex in erat imperdiet molestie a ut mi. Integer accumsan ligula orci, sed tincidunt sapien pellentesque et. Mauris sit amet sagittis quam. Duis id vestibulum orci. Quisque eget lacinia arcu. Pellentesque auctor sollicitudin scelerisque.</p>
+            <p>Proin pellentesque ultrices tellus, a blandit turpis luctus ac. Suspendisse pellentesque, lorem non commodo dignissim, libero lacus lobortis leo, pretium pharetra lectus elit in mauris. Nulla et tortor justo. Curabitur interdum arcu non lacinia lobortis. Phasellus ultricies dictum sodales. Fusce nec consectetur lectus, sed facilisis mauris. Vestibulum elementum sollicitudin neque a fringilla. Donec a sem nisl. Ut sollicitudin quam a urna pretium imperdiet. Maecenas vel placerat lorem. Donec finibus leo nisl, nec faucibus nunc eleifend sed. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;    </p>
+            <p>Phasellus in dolor a odio suscipit feugiat. Duis imperdiet, risus at placerat egestas, diam elit placerat turpis, quis ultrices dolor ante ac turpis. Proin posuere enim aliquet sodales laoreet. Aliquam in dui et lacus rhoncus vestibulum. Praesent urna neque, hendrerit eu convallis a, suscipit in purus. Maecenas mi quam, venenatis at nibh at, pharetra aliquam enim. Donec ligula erat, ullamcorper ac dignissim sed, fermentum quis est. Suspendisse tempor at neque ac blandit. Sed id blandit purus, at dapibus justo.</p>
+        </div>
+    )
+}
---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const Contact = () => {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <p>Phasellus in dolor a odio suscipit feugiat. Duis imperdiet, risus at placerat egestas, diam elit placerat turpis, quis ultrices dolor ante ac turpis. Proin posuere enim aliquet sodales laoreet. Aliquam in dui et lacus rhoncus vestibulum. Praesent urna neque, hendrerit eu convallis a, suscipit in purus. Maecenas mi quam, venenatis at nibh at, pharetra aliquam enim. Donec ligula erat, ullamcorper ac dignissim sed, fermentum quis est. Suspendisse tempor at neque ac blandit. Sed id blandit purus, at dapibus justo.</p>
+        </div>
+    )
+}
